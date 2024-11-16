1baa6791296d7b801498f3970f23ed89e0db2f0d
SachinSrestha <sachinsrestha934@gmail.com>
2024-11-16 11:35:31 +0545

    Updated session

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,10 +1,10 @@
 import jwt from "jsonwebtoken";
 
 export const generate_jwt = (id,res)=>{
-    const token = jwt.sign({id}, process.env.JWT_TOKEN, {expiresIn:"10s"});    
+    const token = jwt.sign({id}, process.env.JWT_TOKEN, {expiresIn:"2d"});    
 
     res.cookie("token", token, {
-        maxAge : 10*1000,
+        maxAge : 2*60*24*60*1000,
         httpOnly:true,
         sameSite:"none",
         secure:false,
